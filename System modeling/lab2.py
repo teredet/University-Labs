9f8981d74aec46e04d41a34829ecb3c45ab8be63
teredet <artem.vanzytler@gmail.com>
2021-09-23 18:40:37 +0300

    Update lab1 and add lab2 on System modeling.

--- System modeling/lab2.py
+++ System modeling/lab2.py
@@ -2,10 +2,10 @@ def module(num1, num2):
     '''
     Реализация одного модуля первого уровня
     '''
-    if num1 and num2: res1 = 1
+    if num1 or num2: res1 = 1
     else: res1 = 0
 
-    if num1 or num2: res2 = 1
+    if num1 and num2: res2 = 1
     else: res2 = 0
 
     return res1, res2
@@ -15,31 +15,26 @@ def level1(x1, x2, x3, x4, x5, x6):
     '''
     Проходимся по каждому ряду первых модулей и получаем по одной переменной
     '''
-
-    # находим x6
-    x1, temp1 = module(x1, x2)
-    x2, temp2 = module(temp1, x3)
-    x3, temp3 = module(temp2, x4)
-    x4, temp4 = module(temp3, x5)
-    x5, x6 = module(temp4, x6)
-
-    # находим x5
-    x1, temp1 = module(x1, x2)
-    x2, temp2 = module(temp1, x3)
-    x3, temp3 = module(temp2, x4)
-    x4, x5 = module(temp3, x5)
-
-    # находим x4
-    x1, temp1 = module(x1, x2)
-    x2, temp2 = module(temp1, x3)
-    x3, x4 = module(temp2, x4)
-
-    # находим x3
-    x1, temp1 = module(x1, x2)
-    x2, x3 = module(temp1, x3)
-
-    # находим x2 and x1
     x1, x2 = module(x1, x2)
+    x3, x4 = module(x3, x4)
+    x5, x6 = module(x5, x6)
+
+    x1, x3 = module(x1, x3)
+    x2, x5 = module(x2, x5)
+    x4, x6 = module(x4, x6)
+
+    # находим x6 и x1
+    x1, x2 = module(x1, x2)
+    x3, x4 = module(x3, x4)
+    x5, x6 = module(x5, x6)
+
+    # находим x5 и x2
+    x2, x3 = module(x2, x3)
+    x4, x5 = module(x4, x5)
+
+    # находим x3 и x4
+    x3, x4 = module(x3, x4)
+    
 
     print("Result after first level:")
     print(x1, x2, x3, x4, x5, x6)
@@ -60,13 +55,13 @@ def level2(x1, x2, x3, x4, x5, x6):
     '''
     Проходимся по модулям второго уровня
     '''    
-    x6 = module2(x6, x5)
-    x5 = module2(x5, x4)
-    x4 = module2(x4, x3)
-    x3 = module2(x3, x2)
-    x2 = module2(x2, x1)
-    x1 = module2(x1, 0)
-
+    x1 = module2(x1, x2)
+    x2 = module2(x2, x3)
+    x3 = module2(x3, x4)
+    x4 = module2(x4, x5)
+    x5 = module2(x5, x6)
+    x6 = module2(x6, 0)
+    
     print("Result after second level:")
     print(x1, x2, x3, x4, x5, x6)
     return x1, x2, x3, x4, x5, x6
@@ -84,15 +79,15 @@ def level3(x1, x2, x3, x4, x5, x6):
     '''
     Проходимся по модулям третьего уровня
     '''    
-    y1 = module3(x1, x2, x3)
-    y2 = module3(x1, x5, x4)
-    y3 = module3(x4, x2, x6)
+    y1 = module3(x1, x5, x3)
+    y2 = module3(x2, x3, x6)
+    y3 = module3(x4, x5, x6)
     print("Result after third level:")
     print(y1, y2, y3)
 
 
 if __name__ == "__main__":
-    #This program implements a triangular processor module.
+    #This program implements a pyramid processor module..
     values = input("Enter values (separated by ' '): ").split(' ')  # присваиваем переменной списко значений, которые вводит пользователь
     x1, x2, x3, x4, x5, x6 = [int(i) for i in values]  # присваиваем переменным значения
     x1, x2, x3, x4, x5, x6 = level1(x1, x2, x3, x4, x5, x6)  # вызывает функцию level1 и присваиваем её значения переменным чтобы передать их в след.функцию
